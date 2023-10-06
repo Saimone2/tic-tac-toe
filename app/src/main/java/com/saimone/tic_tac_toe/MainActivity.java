234e1c25605afcb6b435361ada2e62b79d059d77
Saimone2 <hav1802200474@gmail.com>
2023-10-06 12:25:48 +0300

    init

---
+++ app/src/main/java/com/saimone/tic_tac_toe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saimone.tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
